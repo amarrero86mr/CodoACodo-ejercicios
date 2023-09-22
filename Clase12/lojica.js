79c3a5172f407b2100314f2ba4ca26a5c8e8fed5
amarrero86mr <amarrero86mr@gmail.com>
2023-09-22 19:31:36 -0300

    agregando practicas 11, 12, 13, 14 - javascript

---
+++ Clase12/lojica.js
@@ -0,0 +1,13 @@
+let confirmacionDatos = confirm("Hola, queremos pedirte unos datos para llenar el formulario, ¿estás de acuerdo?");
+
+let name1 = prompt("¿Cómo te llamas?");
+console.log("nombre: " + name1);
+
+let edad = prompt("¿Qué esdad tienes?");
+console.log("Edad: " + edad);
+
+let localidad = prompt("¿cuál es la localidad donde vives?");
+console.log("Localida: " + localidad);
+
+let cusrando = prompt("¿Estás cursando?");
+console.log("Cursando: " + cusrando);
---
+++ Clase13/lojica13.js
@@ -0,0 +1,11 @@
+
+let nameuser = "toto77";
+let passwordUser = "1234";
+let name = prompt("escribe tu nombre de usuario");
+let password = prompt("escribe tu contraseña");
+if (name != nameuser || password != passwordUser) {
+    alert(" nombre de usuario y/o contraseña incorrecto");
+}else{
+    alert("identidad confirmada");
+}
+
