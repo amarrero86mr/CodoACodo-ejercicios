ae23e08307e9faa996fc65db5423c15a1ba9e0ec
amarrero86mr <amarrero86mr@gmail.com>
2023-11-16 17:49:41 -0300

    subiendo practica 26 y restructurando archivos

---
+++ backEnd/Clase 26/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello Word')
+})
+
+app.listen(3000, function(err){
+    if (err) throw err
+    console.log('Servidor web ONLINE')
+})
