1a5277892387d168681f0c01b4d796c7bba4ab2f
amarrero86mr <amarrero86mr@gmail.com>
2023-11-25 17:15:17 -0300

    configuracion inicial practica de clase 30

---
+++ backEnd/Clase 30/src/model/datos.js
@@ -0,0 +1,111 @@
+const datosFalsos = [
+    {
+        id: 1,
+        nombre: "Juan Pérez",
+        email: "juan.perez@example.com",
+        telefono: "+54 9 11 1234 5678",
+        edad: 28,
+        ciudad: "Buenos Aires",
+        imagen: "https://via.placeholder.com/150?text=Juan"
+    },
+    {
+        id: 2,
+        nombre: "María García",
+        email: "maria.garcia@example.com",
+        telefono: "+54 9 11 2345 6789",
+        edad: 35,
+        ciudad: "Córdoba",
+        imagen: "https://via.placeholder.com/150?text=María"
+    },
+    {
+        id: 3,
+        nombre: "Carlos Rodríguez",
+        email: "carlos.rodriguez@example.com",
+        telefono: "+54 9 11 3456 7890",
+        edad: 22,
+        ciudad: "Rosario",
+        imagen: "https://via.placeholder.com/150?text=Carlos"
+    },
+    {
+        id: 4,
+        nombre: "Ana Martínez",
+        email: "ana.martinez@example.com",
+        telefono: "+54 9 11 4567 8901",
+        edad: 29,
+        ciudad: "Mendoza",
+        imagen: "https://via.placeholder.com/150?text=Ana"
+    },
+    {
+        id: 5,
+        nombre: "Javier López",
+        email: "javier.lopez@example.com",
+        telefono: "+54 9 11 5678 9012",
+        edad: 31,
+        ciudad: "Salta",
+        imagen: "https://via.placeholder.com/150?text=Javier"
+    },
+    {
+        id: 6,
+        nombre: "Laura Sánchez",
+        email: "laura.sanchez@example.com",
+        telefono: "+54 9 11 6789 0123",
+        edad: 26,
+        ciudad: "San Juan",
+        imagen: "https://via.placeholder.com/150?text=Laura"
+    },
+    {
+        id: 7,
+        nombre: "Roberto Ramírez",
+        email: "roberto.ramirez@example.com",
+        telefono: "+54 9 11 7890 1234",
+        edad: 33,
+        ciudad: "La Plata",
+        imagen: "https://via.placeholder.com/150?text=Roberto"
+    },
+    {
+        id: 8,
+        nombre: "Sofía González",
+        email: "sofia.gonzalez@example.com",
+        telefono: "+54 9 11 8901 2345",
+        edad: 27,
+        ciudad: "Misiones",
+        imagen: "https://via.placeholder.com/150?text=Sofía"
+    },
+    {
+        id: 9,
+        nombre: "Miguel Fernández",
+        email: "miguel.fernandez@example.com",
+        telefono: "+54 9 11 9012 3456",
+        edad: 30,
+        ciudad: "Neuquén",
+        imagen: "https://via.placeholder.com/150?text=Miguel"
+    },
+    {
+        id: 10,
+        nombre: "Elena Ruiz",
+        email: "elena.ruiz@example.com",
+        telefono: "+54 9 11 0123 4567",
+        edad: 24,
+        ciudad: "Tucumán",
+        imagen: "https://via.placeholder.com/150?text=Elena"
+    },
+    {
+        id: 11,
+        nombre: "Diego Silva",
+        email: "diego.silva@example.com",
+        telefono: "+54 9 11 1234 5678",
+        edad: 32,
+        ciudad: "Bahía Blanca",
+        imagen: "https://via.placeholder.com/150?text=Diego"
+    },
+    {
+        id: 12,
+        nombre: "Valeria López",
+        email: "valeria.lopez@example.com",
+        telefono: "+54 9 11 2345 6789",
+        edad: 29,
+        ciudad: "San Luis",
+        imagen: "https://via.placeholder.com/150?text=Valeria"
+    }
+];
+
